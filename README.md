ee5a19bc86829532d7f68280e16195e2e50d8ed0
ShivamKumarJha <jha.shivam3@gmail.com>
2021-11-20 21:08:24 +0530

    X00T begins
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- README.md
+++ README.md
@@ -1,46 +1,25 @@
-### This repository contains device configuration for Xiaomi-SDM660 devices
+# Device Tree for Asus Zenfone Max Pro M1
+The Asus Zenfone Max Pro M1 (codenamed _"ASUS_X00TD"_) is a mid-range smartphone from Asus.
+It was announced in April 2018 and released in May 2018.
 
-Basic   | Spec Sheet
--------:|:----------
-CPU     | Quad-core 1.84 GHz Qualcomm Kryo 260 LP
-CPU     | Quad-core 2.20 GHz Qualcomm Kryo 260 HP
-Chipset | Qualcomm Snapdragon 660, SDM660
-GPU     | Adreno 512
-ROM     | 32/64/128GB
-RAM     | 3/4/6GB
-Android | 8.1.0
+## Device specifications
 
-## Notes:
- - Depends on: android_packages_resources_devicesettings from LineageOS
- - Depends on: LA.QSSI.11.0.r1-09100-qssi.0 tag for vendor_codeaurora_telephony
- - Depends on: LA.UM.9.6.2.r1-03600-89xx.0 tag for GPS and WFD
+| Device                  | Asus Zenfone Max Pro M1                                     |
+| ----------------------- | :---------------------------------------------------------- |
+| SoC                     | Qualcomm SDM660 Snapdragon 636                              |
+| CPU                     | 8x Qualcomm® Kryo™ 260 CPU                                  |
+| GPU                     | Adreno 509                                                  |
+| Memory                  | 3GB / 4GB RAM / 6GB (LPDDR4X)                               |
+| Shipped Android version | 8.1.0                                                       |
+| Storage                 | 64GB eMMC 5.1 flash storage                                 |
+| MicroSD                 | Up to 2 TB                                                  |
+| Battery                 | Non-removable Li-Po 5000 mAh                                |
+| Dimensions              | 159 x 76 x 8.5 mm                                           |
+| Display                 | 2160 x 1080 (18:9), 5.99 inch                               |
+| Rear camera 1           | 13MP, 1.12-micron pixels, f/2.2 Dual LED flash              |
+| Rear camera 2           | 5MP, 1.12-micron pixels, f/2.4                              |
+| Front camera            | 8MP, 1-micron pixels, f/2.2 1080p 30 fps video, Selfie-light|
 
-## Currently Supported Devices:
- - clover (MI PAD 4 /4 Plus)
- - jason (Redmi Note 3)
- - jasmine_sprout (MI A2)
- - lavender (Redmi Note 7)
- - platina (Mi 8 Lite)
- - tulip (Redmi Note 6 Pro)
- - wayne (MI 6X)
- - whyred (Redmi Note 5)
+## Device picture
 
-## Copyright
-
-```
-#
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2018-2021 Xiaomi-SDM660 Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+![Asus Zenfone Max Pro M1](https://github.com/Navin136/Navin136/blob/main/X00TD.jpg)
--- extract-files.sh
+++ extract-files.sh
@@ -1,55 +1,37 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 Paranoid Android
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-DEVICE_COMMON=sdm660-common
-VENDOR=xiaomi
+DEVICE=X00T
+VENDOR=asus
 
 # Load extract_utils and do some sanity checks
-COMMON_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$COMMON_DIR" ]]; then COMMON_DIR="$PWD"; fi
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-if [[ -z "$DEVICE_DIR" ]]; then
-    DEVICE_DIR="${COMMON_DIR}/../${DEVICE}"
-fi
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-ROOT="$COMMON_DIR"/../../..
-
-HELPER="$ROOT"/tools/extract-utils/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
-ONLY_COMMON=false
-ONLY_DEVICE=false
+
+KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -o | --only-common )
-                ONLY_COMMON=true
-                ;;
-        -d | --only-device )
-                ONLY_DEVICE=true
-                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -67,58 +49,27 @@ while [ "${#}" -gt 0 ]; do
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 function blob_fixup() {
     case "${1}" in
-
-    lib64/libwfdnative.so)
-        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
-        ;;
-
-    vendor/bin/mlipayd@1.1)
-        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
-        ;;
-
-    vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
-        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
-        sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
-        ;;
-
-    vendor/lib64/libril-qc-hal-qmi.so)
-        "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-        ;;
-
-    vendor/lib64/libwvhidl.so)
-        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
-
+        lib64/libwfdnative.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+        vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+      	    ;; 	
     esac
-
-    device_blob_fixup "$@"
 }
 
-if ! typeset -f device_blob_fixup > /dev/null; then
-    device_blob_fixup() {
-        :
-    }
-fi
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-# Initialize the common helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ROOT" true $CLEAN_VENDOR
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-if [[ "$ONLY_DEVICE" = "false" ]] && [[ -s "${COMMON_DIR}"/proprietary-files.txt ]]; then
-    extract "$COMMON_DIR"/proprietary-files.txt "$SRC" "${KANG}" --section "${SECTION}"
-    extract "$COMMON_DIR"/proprietary-files-ir.txt "$SRC" "${KANG}" --section "${SECTION}"
-fi
-if [[ "$ONLY_COMMON" = "false" ]] && [[ -s "${DEVICE_DIR}"/proprietary-files.txt ]]; then
-    if [[ ! "$IS_COMMON" = "true" ]]; then
-        IS_COMMON=false
-    fi
-    # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$ROOT" "$IS_COMMON" "$CLEAN_VENDOR"
-    extract "${DEVICE_DIR}"/proprietary-files.txt "$SRC" "${KANG}" --section "${SECTION}"
-fi
-
-"$COMMON_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
