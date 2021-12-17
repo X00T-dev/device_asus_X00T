fed2806928ec33a44bc281aa6729497e6e3a43e3
Navin136 <nkwhitehat@gmail.com>
2021-12-17 22:20:16 +0530

    Bringup Arrow OS
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- arrow_X00T.mk
+++ arrow_X00T.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow-OS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Set BootAnimation Resolution
 TARGET_BOOT_ANIMATION_RES := 720
@@ -30,7 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_X00T
+PRODUCT_NAME := arrow_X00T
 PRODUCT_MODEL := Zenfone Max Pro M1
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
