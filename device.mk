f43875ade8ec600bdd27fe24c890eda37121ecfc
Navin136 <nkwhitehat@gmail.com>
2022-04-23 19:15:03 +0530

    X00T: Drop Copying ANT Permissions
    * Deprecated

--- device.mk
+++ device.mk
@@ -90,10 +90,6 @@ PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
-# ANT Permission
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # AuthSecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service
